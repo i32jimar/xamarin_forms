293638fef084e3e71912d5db442a7098d909c873
rj_inicial <rafa.jimar@hotmail.com>
2020-07-28 08:43:25 +0200

    Agregar archivos de proyecto.

---
+++ App2.Android/CustomEntry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App2.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//para hacer extensible la propiedad a todos los Entry en Android
+[assembly: ExportRenderer(typeof(Entry),typeof(CustomEntry))]
+namespace App2.Droid
+{
+    public class CustomEntry : EntryRenderer
+    {
+        
+        //pasamos el parametro context a la clase base
+        public CustomEntry(Context context) : base(context)
+        {
+
+        }
+
+        
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control ==null || e.NewElement ==null)
+            {
+                return;
+            }
+            Control.Background = null;
+            if (!e.NewElement.IsPassword )
+            {
+                Control.InputType = Android.Text.InputTypes.TextFlagNoSuggestions; 
+            }
+        }
+    }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {//cambiamos el borde de color 
+            Rectangle_Usuario.Stroke = Color.Black;
+        }
+
+        private void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+            Rectangle_Usuario.Stroke = Color.Gray;
+        }
+    }
+}
